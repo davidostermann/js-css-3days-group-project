08e4ceefe51cf22c4e2b6abbc9f366456de7115a
david <ostermann.david@gmail.com>
2017-02-06 11:29:43 +0100

    resolution

--- data/data.js
+++ data/data.js
@@ -1,5 +1,5 @@
 var contenu = {
-  name: 'La bonne bouffe',
+  name: 'La grande bouffe',
   description: 'Venez goûter nos plats préparés avec amours et bienveillance. Notre chef Gertrude viendra probablement vous serrer la main. Vous pourrez échanger avec cette passionnée de cuisine et repartir avec des conseils pour reproduire sa cuisine inimitable',
   images: [
     'https://media-cdn.tripadvisor.com/media/photo-s/02/1b/9a/bf/le-grande-bouffe.jpg',
@@ -13,22 +13,28 @@ var contenu = {
   ville: 'Montreuil',
   carte: [
     {
+      id: 1,
       name: 'Poulet aux morilles',
       image: 'http://www.epicurien.be/img/recettes-cuisines/20130928_poulet_morilles-500.jpg',
       description: 'Un grand classique le poulet de Bresse aux morilles et à la crème. Si vous ne trouvez pas de volaille de Bresse, choisissez un bon poulet fermier, élevé aux grains et qui a vécu en liberté dans les champs.',
-      price: '23 €'
+      price: '23 €',
+      selected: 0
     },
     {
+      id: 2,
       name: 'Tagine aux pruneaux',
       image:'https://i1.wp.com/cotesoleils.fr/wp-content/uploads/2015/06/tajine-agneau-et-pruneaux-algc3a9rie1.jpg',
       description: 'Un des plus célèbres Tajines Marocains, servi avec une délicieuse viande de veau et des pruneaux issus de l\'agriculture biologique',
-      price: '18 €'
+      price: '18 €',
+      selected: 0
     },
     {
+      id: 3,
       name: 'Salade de saison',
       image: 'http://scrat.hellocoton.fr/img/guide/recette-ma-salade-tiede-de-chou-kale-et-lentilles-patates-douces-et-saumon-fume-18911644.jpg',
       description: 'Toujours à l\'heure, la salade de saison vous régalera avec ses légumes',
-      price: '14 €'
+      price: '14 €',
+      selected: 0
     },
   ]
 }
